1255285d72d49149e975f60d45104fb937c2db78
gudakarthik <gudakarthik2000@gmail.com>
2025-08-25 14:29:16 +0530

    Modified MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import java.nio.charset.Charset
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +50,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var showWebView by remember { mutableStateOf(false) }
+    var context = LocalContext.current
 
     Column(modifier = Modifier.padding(10.dp)) {
 
@@ -42,14 +60,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             modifier = modifier
         )
 
-        Text(
-            text = "Connect Project to github",
-            modifier = modifier
-        )
-        
-        Text(text = "Text has been updated please update this to github")
-
         Text(text = "Added to commit changes in github")
+
+        if (!showWebView) {
+            Button(onClick = {
+                showWebView = true
+            }, modifier = Modifier.padding(15.dp)) {
+                Text(text = "Click here to make payment")
+            }
+        }else{
+            PaymentWebView(url = "http://test-consumer.meghagas.in/consumerRecharge/hesRecharge", crn = "1110100001", amount = "200")
+        }
     }
 
 
@@ -61,4 +82,13 @@ fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun PaymentWebView(url : String, crn : String, amount : String){
+    
+    Column {
+        Text(text = "Now you can make your payments in app itself")
+    }
+    
 }
